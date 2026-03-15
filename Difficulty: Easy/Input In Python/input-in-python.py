40cf58da640cf9033c1948cea95ac235fff70ce4
Parthajit3080 <121250517+Parthajit3080@users.noreply.github.com>
2026-03-16 01:51:10 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Input In Python/input-in-python.py
@@ -0,0 +1,19 @@
+# User function Template for python3
+
+########### Write your code below ###############
+
+# Take string input and print the string
+
+# Take integer input and add 10 to the integer input and print
+
+# Take floating-point input and multiply the float input by 10 and print
+
+########### Write your code above ###############
+
+s=input()
+n=int(input())
+f=float(input())
+
+print(s)
+print(n+10)
+print(f*10)
