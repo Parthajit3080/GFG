7d4d42ef7ca467546c0786c922b2ad928ca3bf8f
Parthajit3080 <121250517+Parthajit3080@users.noreply.github.com>
2026-03-16 01:50:48 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Int Str/int-str.py
@@ -0,0 +1,18 @@
+#User function Template for python3
+a = input()
+b = input()
+c = input()
+# Write your code below that prints a a times and b b times, seperated by c
+i=1
+j=1
+a=int(a)
+b=int(b)
+
+
+for i in range(a):
+    print(a,end="")
+    
+print(c,end="")
+
+for j in range(b):
+    print(b,end="")
