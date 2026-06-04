684059b9f4253f9550ce0e43f2d91815111eb2ce
Parthajit3080 <121250517+Parthajit3080@users.noreply.github.com>
2026-06-05 04:14:39 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Butterfly Pattern/butterfly-pattern.cpp
@@ -0,0 +1,26 @@
+class Solution {
+  public:
+    void printButterfly(int n) {
+        // code here
+        for(int i =1;i<=n;i++){
+        for(int j=1;j<=i;j++)
+            printf("*");
+        for(int j=1;j<=2*(n-i)-1;j++)
+            printf(" ");
+        
+        int start= (i==n)?2:1;
+        for(int j=start;j<=i;j++)
+            printf("*");       
+        printf("\n");
+    }
+    for(int i =n-1;i>=1;i--){
+        for(int j=1;j<=i;j++)
+            printf("*");
+        for(int j=1;j<=2*(n-i)-1;j++)
+            printf(" ");
+        for(int j=1;j<=i;j++)
+            printf("*");       
+        printf("\n");
+    }
+    }
+};
